d713efdbd206a78ed5cd89fe803bdc3920f4175d
piplup14 <rukai@bk.ru>
2021-10-30 22:30:31 +0300

    first commit

---
+++ hello/app/controller/users.go
@@ -0,0 +1,24 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+	"prog/gotry/hello/app/model"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func GetUsers(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	//получаем список всех пользователей
+	users, err := model.GetAllUsers()
+	if err != nil {
+		http.Error(rw, err.Error(), 400)
+		return
+	}
+	//возвращаем список клиенту в формате JSON
+	err = json.NewEncoder(rw).Encode(users)
+	if err != nil {
+		http.Error(rw, err.Error(), 400)
+		return
+	}
+}
