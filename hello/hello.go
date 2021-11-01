fff80e60b77ebb857082516bf42673e0ccf41bdc
piplup14 <rukai@bk.ru>
2021-11-01 21:50:32 +0300

    list from db

--- hello/hello.go
+++ hello/hello.go
@@ -3,28 +3,70 @@ package main
 import (
 	"net/http"
 
+	"database/sql"
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
 
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+type img struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Date string `json:"date"`
+	Path string `json:"path"`
 }
 
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "admin"
+	dbname   = "postgres"
+)
+
+var images = []img{}
 
 func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	// open database
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+
+	// close database
+	defer db.Close()
+
+	// check db
+	err = db.Ping()
+	CheckError(err)
+
+	fmt.Println("Connected!")
+
+	rows, err := db.Query(`SELECT "name", "id" , "date", "path" FROM "images"`)
+	CheckError(err)
+	var newItem img
+	defer rows.Close()
+	for rows.Next() {
+		var name string
+		var id int
+		var date string
+		var path string
+
+		err = rows.Scan(&name, &id, &date, &path)
+		CheckError(err)
+		newItem = img{id, name, date, path}
+		fmt.Println(newItem)
+		images = append(images, newItem)
+	}
+
+	CheckError(err)
+
+	c.IndentedJSON(http.StatusOK, images)
 }
 
 func postAlbums(c *gin.Context) {
-	var newAlbum album
+	var newAlbum img
 
 	// Call BindJSON to bind the received JSON to
 	// newAlbum.
@@ -33,14 +75,21 @@ func postAlbums(c *gin.Context) {
 	}
 
 	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
+	images = append(images, newAlbum)
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
+func CheckError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
+
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
 	router.POST("/albums", postAlbums)
 
-	router.Run("localhost:8080")
+	router.Run("localhost:8980")
 }
