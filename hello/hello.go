d4db2e3b4696022138754e577e3d2ad012fa6f59
piplup14 <rukai@bk.ru>
2021-11-13 17:57:21 +0300

    input file

--- hello/hello.go
+++ hello/hello.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"net/http"
-
 	"database/sql"
 	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
+	"github.com/minio/minio-go"
 )
 
 type img struct {
@@ -25,8 +27,11 @@ const (
 	dbname   = "postgres"
 )
 
+var ids = []int{}
+
 var images = []img{}
 
+//GET ALL-----------------------------------------------------
 func getAlbums(c *gin.Context) {
 	images = []img{}
 	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
@@ -48,6 +53,7 @@ func getAlbums(c *gin.Context) {
 	CheckError(err)
 	var newItem img
 	defer rows.Close()
+
 	for rows.Next() {
 		var name string
 		var id int
@@ -58,7 +64,9 @@ func getAlbums(c *gin.Context) {
 		CheckError(err)
 		newItem = img{id, name, date, path}
 		fmt.Println(newItem)
+		ids = append(ids, newItem.ID)
 		images = append(images, newItem)
+
 	}
 
 	CheckError(err)
@@ -66,6 +74,7 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, images)
 }
 
+//ADD-----------------------------------------------------------
 func postAlbums(c *gin.Context) {
 	var newAlbum img
 
@@ -80,15 +89,62 @@ func postAlbums(c *gin.Context) {
 	db, err := sql.Open("postgres", psqlconn)
 	CheckError(err)
 	defer db.Close()
+	var n = rand.Int()
+	for i := 0; i < len(ids); i++ {
+		if n == ids[i] {
+			n = rand.Int()
+		}
+	}
 
 	images = append(images, newAlbum)
+
+	var id = rand.Int()
 
 	result, err := db.Exec("insert into images (path, date, name, id) values ($1, $2, $3, $4)", newAlbum.Path, newAlbum.Date, newAlbum.Name, newAlbum.ID)
 	CheckError(err)
 
-	fmt.Println(result.LastInsertId())
 	fmt.Println(result.RowsAffected())
 
+	//minio part --------------------------------------------------------------
+	endpoint := "play.minio.io:9000"
+	accessKeyID := "Q3AM3UQ867SPQQA43P2F"
+	secretAccessKey := "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"
+	useSSL := true
+
+	// Initialize minio client object.
+	minioClient, err := minio.New(endpoint, accessKeyID, secretAccessKey, useSSL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Make a new bucket called mymusic.
+	bucketName := "imagesapi"
+	location := "us-east-1"
+
+	err = minioClient.MakeBucket(bucketName, location)
+	if err != nil {
+		// Check to see if we already own this bucket (which happens if you run this twice)
+		exists, err := minioClient.BucketExists(bucketName)
+		if err == nil && exists {
+			log.Printf("We already own %s\n", bucketName)
+		} else {
+			log.Fatalln(err)
+		}
+	} else {
+		log.Printf("Successfully created %s\n", bucketName)
+	}
+
+	// Upload the file
+	filePath := newAlbum.Path
+	contentType := "image/jpeg"
+
+	// Upload the file with FPutObject
+	s, err := minioClient.FPutObject(bucketName, newAlbum.Name, filePath, minio.PutObjectOptions{ContentType: contentType})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("Successfully uploaded %s of size %d\n", newAlbum.Name, s)
 }
 
 func CheckError(err error) {
@@ -97,6 +153,7 @@ func CheckError(err error) {
 	}
 }
 
+//SERVER-------------------------------------------------------------
 func main() {
 
 	router := gin.Default()
